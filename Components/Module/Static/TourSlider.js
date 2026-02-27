643a62ca32ad428016f1b1dfde1d250b47695b40
Parichehr <alfa@Caroline-Adkins.local>
2026-02-27 00:08:10 -0800

    feat : static images  buttom

--- Components/Module/Static/TourSlider.js
+++ Components/Module/Static/TourSlider.js
@@ -3,6 +3,7 @@
 import { useState } from "react";
 import styles from "./TourSlider.module.css";
 import Image from "next/image";
+import { GoArrowRight, GoArrowLeft } from "react-icons/go";
 
 const images = [
   "/SVG/static/R (1).svg",
@@ -23,41 +24,59 @@ export default function TourSlider() {
   };
 
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.stack}>
-        {images.map((img, index) => {
-          const position = (index - active + images.length) % images.length;
+    <div className={styles.staticImg}>
+      <div className={styles.headqus}>
+        <div className={styles.qus}>
+          <Image
+            src="/SVG/static/Group 7.svg"
+            alt="phone"
+            width={34}
+            height={38}
+          />
+          <Image src="/SVG/static/؟.svg" alt="phone" width={10} height={20} className={styles.question} />
+        </div>
+        <div className={styles.head}>
+          <h1>
+            چرا <span>تورینو</span> ؟
+          </h1>
+        </div>
+      </div>
+      <div className={styles.wrapper}>
+        <div className={styles.stack}>
+          {images.map((img, index) => {
+            const position = (index - active + images.length) % images.length;
 
-          return (
-           <div
-  key={index}
-  className={styles.card}
-  style={{
-    transform: `
+            return (
+              <div
+                key={index}
+                className={styles.card}
+                style={{
+                  transform: `
       translateX(${-position * 35}px)
       translateY(${position * 0}px)
-      scale(${1 - position * 0.05})
+      scaleY(${1 - position * 0.1})
     `,
-    zIndex: images.length - position,
-  }}
->
-  <Image
-    src={img}
-    alt=""
-    fill
-    className={styles.image}
-  />
-</div>
-          );
-        })}
-      </div>
+                  zIndex: images.length - position,
+                }}
+              >
+                <Image src={img} alt="" fill className={styles.image} />
+              </div>
+            );
+          })}
+        </div>
 
-      <div className={styles.controls}>
-        <button onClick={prev}>‹</button>
-        <span>
-          {active + 1} / {images.length}
-        </span>
-        <button onClick={next}>›</button>
+        <div className={styles.controls}>
+          <button onClick={prev}>
+            {" "}
+            <GoArrowRight />
+          </button>
+          <span>
+            {active + 1} / {images.length}
+          </span>
+          <button onClick={next}>
+            <GoArrowLeft />
+          </button>
+        </div>
       </div>
     </div>
   );
